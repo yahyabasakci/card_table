674497a89cb8b3dd3693d9cce3db1b093c8bd80b
yahyabasakci <yahyabasakcii@gmail.com>
2024-08-05 08:54:32 -0400

    05-08-2024

---
+++ config/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
