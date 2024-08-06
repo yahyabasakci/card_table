32c5993bbef9f2e6e85dd7b8d3fe2b1be805413c
yahyabasakci <yahyabasakcii@gmail.com>
2024-08-06 09:41:13 -0400

    06-08-2024

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
      @dynamic_columns = Column.where(is_static: false)
      @cards=Card.all
      ############
+     @card=Card.new
+     @user=current_user
+     #
      @column_card_counts = (@static_columns + @dynamic_columns).each_with_object({}) do |column, counts|
       counts[column.id] = @cards.where(column_id: column.id).count
     end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,35 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+
+
+// app/javascripts/modal.js
+
+// JavaScript for modal functionality
+// app/assets/javascripts/modal.js
+document.addEventListener('DOMContentLoaded', () => {
+    const openModalButton = document.querySelector('#openModalButton');
+    const modal = document.querySelector('#cardModal');
+    const closeModalButton = document.querySelector('#closeModal');
+  
+    openModalButton.addEventListener('click', (event) => {
+      event.preventDefault(); // Linkin varsayılan işlevini engelle
+      modal.classList.remove('hidden');
+    });
+  
+    closeModalButton.addEventListener('click', () => {
+      modal.classList.add('hidden');
+    });
+  
+    window.addEventListener('click', (event) => {
+      if (event.target === modal) {
+        modal.classList.add('hidden');
+      }
+    });
+
+  });
+  
+  
+  
